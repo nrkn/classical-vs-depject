efc6604fd961bdfe2090b8b9992f3e9b8946ad6d
Nik Coughlin <nrkn.com@gmail.com>
2017-03-10 17:14:16 +1300

    Correct api has wrong precedence

--- depject/api/index.js
+++ depject/api/index.js
@@ -3,16 +3,14 @@
 const combine = require( 'depject' )
 const common = require( './common' )
 
-const Api = ( adapter, ...plugins ) => {
-  const sockets = combine( [ adapter, common, ...plugins ] )
-
-  const api = Object.keys( sockets ).reduce( ( api, key ) => {
+const socketsToApi = sockets =>
+  Object.keys( sockets ).reduce( ( api, key ) => {
     api[ key ] = sockets[ key ][ 0 ]
 
     return api
   }, {})
 
-  return api
-}
+const Api = ( adapter, ...plugins ) =>
+  socketsToApi( combine( [ ...plugins, common, adapter ] ) )
 
 module.exports = Api
